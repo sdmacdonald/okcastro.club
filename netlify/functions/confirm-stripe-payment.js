a5bb371dde9a6ad82ddf3bd303dd4183ea075f61
Danny MacDonald <danny@dannymacdonald.me>
2023-01-31 18:44:46 -0600

    ...

--- netlify/functions/confirm-stripe-payment.js
+++ netlify/functions/confirm-stripe-payment.js
@@ -1,11 +1,14 @@
+// Webhook that listens for events sent from Stripe
+// Requires configuration in the Stripe Dashboard
+// For more information read https://stripe.com/docs/webhooks
 require("dotenv").config();
 
-const stripe = require("stripe")(import.meta.env.STRIPE_SK);
+const stripe = require("stripe")(process.env.STRIPE_SK);
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(import.meta.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async (event, context) => {
-  const endpointSecret = import.meta.env.STRIPE_ES;
+  const endpointSecret = process.env.STRIPE_ES;
   const sig = event.headers["stripe-signature"];
 
   let stripeEvent = await stripe.webhooks.constructEvent(
@@ -17,10 +20,10 @@ exports.handler = async (event, context) => {
   const { id, metadata } = await event.body.data.object;
 
   const msg = {
-    to: import.meta.env.SENDGRID_TO,
-    cc: import.meta.env.SENDGRID_CC,
-    bcc: import.meta.env.SENDGRID_BCC,
-    from: import.meta.env.SENDGRID_FROM,
+    to: process.env.SENDGRID_TO,
+    cc: process.env.SENDGRID_CC,
+    bcc: process.env.SENDGRID_BCC,
+    from: process.env.SENDGRID_FROM,
     subject: `New Club Member: ${metadata.name}`,
     text: `${id}: We have a new club member. Data captured: ${metadata}.`,
     html: `<html><body><p>${metadata}</p></body></html>`,
