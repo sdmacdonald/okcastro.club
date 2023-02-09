1e388bf983d0df2f8b5da9341ba288f6c74c5abe
Danny MacDonald <danny@dannymacdonald.me>
2023-02-09 08:27:51 -0600

    price update; remove stripe fee

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -34,8 +34,8 @@ exports.handler = async (event, context) => {
     switch (item) {
       case "membership":
         const items = [9, 6, 3, 36, 33, 30, 27, 24, 21, 18, 15, 12];
-        let i = items[new Date().getMonth()] * 100;
-        price = Math.round((i + 30) / (1 - 0.029));
+        price = items[new Date().getMonth()] * 100;
+        // price = Math.round((i + 30) / (1 - 0.029));
         break;
       default:
         price = 3600;
