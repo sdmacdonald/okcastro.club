86e589a3826e7618156be9a0eb27cb5143a4de32
sdmacdonald <123282398+sdmacdonald@users.noreply.github.com>
2025-01-02 19:37:59 -0600

    Update getPrice.js

--- src/assets/data/getPrice.js
+++ src/assets/data/getPrice.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "@chakra-ui/react";
 
 export const getPrice = () => {
-  const months = [9, 6, 3, 36, 33, 30, 27, 24, 21, 18, 15, 12];
+  const months = [45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12];
   let m = months[new Date().getMonth()];
   return m;
 };
